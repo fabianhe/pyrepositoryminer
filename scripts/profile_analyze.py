70ec3682d0d3111068d637dc84b44ea84f84f9e3
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-06-06 09:53:53 +0200

    Add profiling scripts

---
+++ scripts/profile_analyze.py
@@ -0,0 +1,23 @@
+from cProfile import Profile
+from pathlib import Path
+from pstats import Stats
+
+from typer import FileText
+
+from pyrepositoryminer.main import AvailableMetrics, analyze
+
+
+def main() -> None:
+    p = Path("/Users/fabian/bare-repos/numpy.git")
+    with open("commits copy.txt", "rb") as f:
+        c = FileText(f)
+        analyze(p, [AvailableMetrics["filecount"]], c)
+
+
+if __name__ == "__main__":
+    profile = Profile()
+    profile.enable()
+    main()
+    profile.disable()
+    profile_stats = Stats(profile).sort_stats("tottime")
+    profile_stats.print_stats()
