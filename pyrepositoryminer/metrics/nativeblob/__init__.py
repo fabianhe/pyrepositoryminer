f7929867d8ee62642839152c8aea048da219e976
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-06-19 16:35:10 +0200

    refactor linecount

--- pyrepositoryminer/metrics/nativeblob/__init__.py
+++ pyrepositoryminer/metrics/nativeblob/__init__.py
@@ -1,6 +1,8 @@
 from typing import Awaitable, Callable, Dict, Iterable, Type
 
 from pyrepositoryminer.metrics.nativeblob.halstead import Halstead
+from pyrepositoryminer.metrics.nativeblob.linecount import Linecount
+from pyrepositoryminer.metrics.nativeblob.maintainability import Maintainability
 from pyrepositoryminer.metrics.nativeblob.raw import Raw
 from pyrepositoryminer.metrics.structs import BlobTuple, Metric
 
@@ -9,4 +11,7 @@ Metrics: Dict[
     Type[Callable[[BlobTuple], Awaitable[Iterable[Metric]]]],
 ]
 
-Metrics = {str(metric.__name__).lower(): metric for metric in (Halstead, Raw)}
+Metrics = {
+    str(metric.__name__).lower(): metric
+    for metric in (Halstead, Raw, Maintainability, Linecount)
+}
