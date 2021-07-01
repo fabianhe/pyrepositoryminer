c1c49ff37fc5f724da85faca210df03efb7ecac1
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-07-01 18:03:44 +0200

    Change docker demo

--- scripts/docker-demo.sh
+++ scripts/docker-demo.sh
@@ -2,6 +2,6 @@
 
 docker create -it --name mycontainer -v $1/:/repo.git myimage
 docker start mycontainer
-docker exec -i mycontainer pyrepositoryminer branch /repo.git | docker exec -i mycontainer pyrepositoryminer commits --limit 1 /repo.git | docker exec -i mycontainer pyrepositoryminer analyze /repo.git
+docker exec -i mycontainer sh -c 'pyrepositoryminer branch /repo.git |  pyrepositoryminer commits /repo.git | pyrepositoryminer analyze /repo.git'
 docker stop mycontainer
 docker rm mycontainer
