a115207160f20bbcc3acb37a06a7a4dab04a8db5
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-07-10 14:06:17 +0200

    Change dockerfile

--- scripts/docker-demo.sh
+++ scripts/docker-demo.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env /bin/bash
 
-docker create -it --name mycontainer -v $1/:/repo.git myimage
-docker start mycontainer
-docker exec -i mycontainer sh -c 'pyrepositoryminer branch /repo.git |  pyrepositoryminer commits /repo.git | pyrepositoryminer analyze /repo.git'
-docker stop mycontainer
-docker rm mycontainer
+docker build . -t myimage && docker run --env-file .env -v $1/:/repo.git --rm myimage sh -c 'pyrepositoryminer branch /repo.git |  pyrepositoryminer commits /repo.git | pyrepositoryminer analyze /repo.git'
