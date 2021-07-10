b638737a96188b85a93dd153b4f4b9c453d4c3e1
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-07-10 11:20:27 +0200

    Change dir metrics class

--- pyrepositoryminer/metrics/dir/main.py
+++ pyrepositoryminer/metrics/dir/main.py
@@ -1,9 +1,8 @@
 from abc import ABC
-from datetime import datetime
 from tempfile import TemporaryDirectory
-from typing import Any, Optional
+from typing import Optional
 
-from pygit2 import Repository
+from pygit2 import GIT_CHECKOUT_FORCE, Repository
 
 from pyrepositoryminer.metrics.main import BaseMetric, BaseVisitor
 from pyrepositoryminer.metrics.structs import DirMetricInput
@@ -19,19 +18,17 @@ class DirVisitor(BaseVisitor):
     def __call__(self, visitable_object: Object) -> Optional[DirMetricInput]:
         if not isinstance(visitable_object, Commit):
             return None
-        refname = f"refs/heads/{datetime.now().timestamp()}_{visitable_object.id}"
-        self.ref: Any = self.repository.references.create(refname, visitable_object.id)
-        self.tempdir: TemporaryDirectory[str] = TemporaryDirectory()
-        self.wtname = f"wt_{visitable_object.id}"
-        path = f"{self.tempdir.name}/{self.wtname}"
-        self.worktree: Any = self.repository.add_worktree(self.wtname, path, self.ref)
+        self.tempdir: TemporaryDirectory[str] = TemporaryDirectory(dir=self.base_dir)
+        self.repository.checkout_tree(
+            visitable_object.tree.obj,
+            directory=self.tempdir.name,
+            strategy=GIT_CHECKOUT_FORCE,
+        )
         is_cached = self.oid_is_cached(visitable_object.tree.id)
         self.cache_oid(visitable_object.tree.id)
-        return DirMetricInput(is_cached, path, visitable_object.tree)
+        return DirMetricInput(is_cached, self.tempdir.name, visitable_object.tree)
 
     def close(self) -> None:
-        self.worktree.prune(True)
-        self.ref.delete()
         self.tempdir.cleanup()
 
 
