dc55259611e9fcd2ed51ec3bf75f66b9cd03603e
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-07-26 12:58:02 +0200

    Change tokei metrics

--- pyrepositoryminer/metrics/diffdir/difftokei.py
+++ pyrepositoryminer/metrics/diffdir/difftokei.py
@@ -1,52 +1,10 @@
-from asyncio import create_subprocess_exec
-from asyncio.subprocess import PIPE
-from functools import reduce
-from json import loads
 from typing import Iterable
 
 from pyrepositoryminer.metrics.diffdir.main import DiffDirMetric
-from pyrepositoryminer.metrics.structs import DirMetricInput, Metric, ObjectIdentifier
-from pyrepositoryminer.pobjects import Tree
-
-
-def descend_tree(tree: Tree, obj_name: str) -> str:
-    return reduce(lambda a, b: a[b], obj_name.split("/"), tree).id  # type: ignore
+from pyrepositoryminer.metrics.dir.tokei import tokei
+from pyrepositoryminer.metrics.structs import DirMetricInput, Metric
 
 
 class DiffTokei(DiffDirMetric):
     async def analyze(self, tup: DirMetricInput) -> Iterable[Metric]:
-        p = await create_subprocess_exec(
-            "tokei", "--output", "json", tup.path, stdout=PIPE
-        )
-        stdout, _ = await p.communicate()
-        data = loads(bytes(stdout).decode("utf-8"))
-        result = [
-            Metric(
-                "tokei",
-                {
-                    "category": category_name,
-                    "blanks": report["stats"]["blanks"],
-                    "code": report["stats"]["code"],
-                    "comments": report["stats"]["comments"],
-                },
-                False,
-                ObjectIdentifier(
-                    descend_tree(tup.tree, report["name"][len(tup.path) + 1 :]),
-                    report["name"][len(tup.path) + 1 :],
-                ),
-            )
-            for category_name, reports in data["Total"]["children"].items()
-            for report in reports
-        ]
-        result.append(
-            Metric(
-                self.name,
-                {
-                    "blanks": data["Total"]["blanks"],
-                    "code": data["Total"]["code"],
-                    "comments": data["Total"]["comments"],
-                },
-                False,
-            )
-        )
-        return result
+        return await tokei(self.name, tup.path, tup.tree)
--- pyrepositoryminer/metrics/dir/tokei.py
+++ pyrepositoryminer/metrics/dir/tokei.py
@@ -5,50 +5,46 @@ from typing import Iterable
 
 from pyrepositoryminer.metrics.dir.main import DirMetric
 from pyrepositoryminer.metrics.structs import DirMetricInput, Metric, ObjectIdentifier
-from pyrepositoryminer.pobjects import Object, Tree
+from pyrepositoryminer.metrics.utils import descend_tree
+from pyrepositoryminer.pobjects import Tree
 
 
-def descend_tree(tree: Tree, obj_name: str) -> str:
-    item: Object = tree
-    for i in obj_name.split("/"):
-        item = item[i]  # type: ignore
-    return item.id
+async def tokei(name: str, path: str, tree: Tree) -> Iterable[Metric]:
+    p = await create_subprocess_exec("tokei", "--output", "json", path, stdout=PIPE)
+    stdout, _ = await p.communicate()
+    data = loads(bytes(stdout).decode("utf-8"))
+    result = [
+        Metric(
+            name,
+            {
+                "category": category_name,
+                "blanks": report["stats"]["blanks"],
+                "code": report["stats"]["code"],
+                "comments": report["stats"]["comments"],
+            },
+            False,
+            ObjectIdentifier(
+                descend_tree(tree, report["name"][len(path) + 1 :]),
+                report["name"][len(path) + 1 :],
+            ),
+        )
+        for category_name, reports in data["Total"]["children"].items()
+        for report in reports
+    ]
+    result.append(
+        Metric(
+            name,
+            {
+                "blanks": data["Total"]["blanks"],
+                "code": data["Total"]["code"],
+                "comments": data["Total"]["comments"],
+            },
+            False,
+        )
+    )
+    return result
 
 
 class Tokei(DirMetric):
     async def analyze(self, tup: DirMetricInput) -> Iterable[Metric]:
-        p = await create_subprocess_exec(
-            "tokei", "--output", "json", tup.path, stdout=PIPE
-        )
-        stdout, _ = await p.communicate()
-        data = loads(bytes(stdout).decode("utf-8"))
-        result = [
-            Metric(
-                "tokei",
-                {
-                    "category": category_name,
-                    "blanks": report["stats"]["blanks"],
-                    "code": report["stats"]["code"],
-                    "comments": report["stats"]["comments"],
-                },
-                False,
-                ObjectIdentifier(
-                    descend_tree(tup.tree, report["name"][len(tup.path) + 1 :]),
-                    report["name"][len(tup.path) + 1 :],
-                ),
-            )
-            for category_name, reports in data["Total"]["children"].items()
-            for report in reports
-        ]
-        result.append(
-            Metric(
-                self.name,
-                {
-                    "blanks": data["Total"]["blanks"],
-                    "code": data["Total"]["code"],
-                    "comments": data["Total"]["comments"],
-                },
-                False,
-            )
-        )
-        return result
+        return await tokei(self.name, tup.path, tup.tree)
