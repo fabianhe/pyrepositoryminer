a78739667dbc653dc05ca64890a2ce3886209d10
fabianhe <39628987+fabianhe@users.noreply.github.com>
2021-07-26 14:10:51 +0200

    Change tokei

--- pyrepositoryminer/metrics/diffdir/difftokei.py
+++ pyrepositoryminer/metrics/diffdir/difftokei.py
@@ -1,10 +1,6 @@
-from typing import Iterable
-
 from pyrepositoryminer.metrics.diffdir.main import DiffDirMetric
-from pyrepositoryminer.metrics.dir.tokei import tokei
-from pyrepositoryminer.metrics.structs import DirMetricInput, Metric
+from pyrepositoryminer.metrics.dir.tokei import Tokei
 
 
 class DiffTokei(DiffDirMetric):
-    async def analyze(self, tup: DirMetricInput) -> Iterable[Metric]:
-        return await tokei(self.name, tup.path, tup.tree)
+    analyze = Tokei.analyze
